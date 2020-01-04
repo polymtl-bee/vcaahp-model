075c49ba6ccedf439991f33ae655d67235679f7e
Gregor Strugala <gregor.strugala@gmail.com>
2020-01-04 17:41:00 +0100

    Add batch script to open the solution in VScode

---
+++ openSLN.bat
@@ -0,0 +1,5 @@
+rem   Open TRNSYS solution in Visual Studio
+
+start^
+ "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"^
+ "C:\TRNSYS18\Compilers\TRNSYS-Ivf2019-Vs2019\TRNSYS-Ivf2019-Vs2019.sln"
